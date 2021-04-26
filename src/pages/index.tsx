af77dd1d38d6c60ffc5f76f047d1357589fb5aeb
YonJun <jhon_08_2007@hotmail.com>
2021-04-25 23:49:35 -0500

    update index page header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,36 @@ import Seo from 'components/Seo';
 import { Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faUser } from '@fortawesome/free-regular-svg-icons';
 
 const MainPage = () => (
   <Fragment>
     <Container>
       <Fragment>
         <Seo title="Home" />
-        <div className="bg-green-500">
+        <div className="">
+          {
+            //Header
+          }
           <div className="grid grid-cols-12">
-            <div className="bg-gray-100 p-4 font-semibold rounded-lg col-span-8 grid grid-cols-12">
+            <div className="col-span-9 bg-gray-100 p-4 font-semibold rounded-lg grid grid-cols-12">
               <div className="col-span-1">
                 <FontAwesomeIcon icon={faSearch} />
               </div>
               <input className=" bg-gray-100 col-span-11 focus:outline-none" placeholder="Search..." />
             </div>
-            <div className="bg-red-600 col-span-4">icons</div>
+            <div className="col-span-3 flex items-center justify-end gap-5">
+              <div>
+                <FontAwesomeIcon icon={faUser} />
+              </div>
+              <div className="p-4 bg-primary-main rounded-lg text-center text-white">
+                <FontAwesomeIcon icon={faBell} />
+              </div>
+            </div>
           </div>
+          {
+            //Body
+          }
         </div>
       </Fragment>
     </Container>
