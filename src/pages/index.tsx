f282b52b9708d5e41d14ff267a11f51e8c96079f
YonJun <jhon_08_2007@hotmail.com>
2021-04-28 23:00:20 -0500

    add blur effect in card footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,29 +36,37 @@ const MainPage = () => (
           <div className="bg-green-500 mt-10">
             <h3 className="font-semibold text-2xl">Your unfinished courses</h3>
             <div className="mt-5 grid grid-flow-col grid-cols-2 gap-4">
-              <div className="overflow-hidden rounded-3xl relative">
-                <div className="absolute w-full h-full cover p-8">
-                  <div className="text-white">
-                    <div className="inline-flex items-center">
-                      <div
-                        className="rounded-full overflow-hidden border-2 border-primary-main"
-                        style={{ width: 45, height: 45, padding: 1.5 }}
-                      >
-                        <img className="rounded-full" src="/user_1.png" width="100%" height="auto" />
+              {[{ key: '1' }, { key: '2' }].map((item) => (
+                <div key={item.key} className="overflow-hidden rounded-3xl relative">
+                  <div className="absolute w-full h-full cover p-8 flex flex-col justify-between">
+                    <div className="text-white">
+                      <div className="inline-flex items-center">
+                        <div
+                          className="rounded-full overflow-hidden border-2 border-primary-main"
+                          style={{ width: 45, height: 45, padding: 1.5 }}
+                        >
+                          <img className="rounded-full" src="/user_1.png" width="100%" height="auto" />
+                        </div>
+                        <div className="ml-5">
+                          <p className="font-bold text-base">Dianne Edwards</p>
+                          <p className="font-normal text-sm">@dianneed</p>
+                        </div>
                       </div>
-                      <div className="ml-5">
-                        <p className="font-bold text-base">Dianne Edwards</p>
-                        <p className="font-normal text-sm">@dianneed</p>
+                      <div className="float-right">
+                        <button className="btn bg-primary-main">82 min</button>
                       </div>
                     </div>
-                    <div className="float-right">
-                      <button className="btn bg-primary-main">82 min</button>
+                    <div
+                      className="text-white relative p-4 rounded-3xl text-lg"
+                      style={{ background: 'rgba(60, 17, 134, 0.3)', backdropFilter: 'blur(10px)' }}
+                    >
+                      Learning how to create simple Swift applications in 8 lessons
                     </div>
                   </div>
+                  <img src="/image_1.png" height="auto" width="100%" />
                 </div>
-                <img src="/image_1.png" height="auto" width="100%" />
-              </div>
-              <div className="bg-red-400">2</div>
+              ))}
+              {/* <div className="bg-red-400">2</div> */}
             </div>
           </div>
         </div>
